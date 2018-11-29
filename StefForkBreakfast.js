e7ecbe88b25e8f9201c4d8fac7fcd70f67b839ec
Stefan Pushkov <stefan.pushkov@icloud.com>
2018-11-29 12:36:37 +0300

    add a transition property to first two items in navbar middle section

--- StefForkBreakfast.js
+++ StefForkBreakfast.js
@@ -1,5 +1,11 @@
 $("#our-rest").click(function(event) {
+	$('.second-levelX').removeClass("left-right");
+	$('.second-level').toggleClass('left-right');
+	event.preventDefault();
+})
 
-	$('.sub-menu').toggleClass('left-right');
+$("#food-menu").click(function(event) {
+	$('.second-level').removeClass("left-right")
+	$('.second-levelX').toggleClass('left-right');
 	event.preventDefault();
 })
